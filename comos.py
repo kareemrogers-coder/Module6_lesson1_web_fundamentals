c5a967dc3d3b5d233061c2bb5db2388a1c42e870
kareemrogers-coder <kareemrogers27@gmail.com>
2024-08-23 20:30:38 -0400

    cosmo commit

--- comos.py
+++ comos.py
@@ -1,29 +1,44 @@
 import requests
 
+
 def fetch_planet_data():
     url = "https://api.le-systeme-solaire.net/rest/bodies/"
     response = requests.get(url)
     planets = response.json()['bodies']
 
-    #process each planet info
-    for planet in planets:
-        if planet['isPlanet']:
-            name = planet.get('englishName', 'Unknown')
-            mass = planet.get('mass', {}).get('massValue', 0) * planet.get('mass', {}).get('massExponent', 0)
-            orbit_period = planet.get('sideralOrbit', 0)
-            print(f"Planet: {name}, Mass: {mass}, Orbit Period: {orbit_period} days")
+    for cosmo in planets:
+        if cosmo['isPlanet']:
+            name = cosmo.get('englishName', 'Unknown')  
+            mass = cosmo.get('mass', {}).get('massValue', 'Unknown') 
+            orbit_period = cosmo.get('sideralOrbit', 'Unknown')  
+            print(f"The Planet in the name of: {name}, has a Mass of {mass}, with an Orbit of  {orbit_period} days")
 
 fetch_planet_data()
 
+def fetch_planet_data():
+    url = "https://api.le-systeme-solaire.net/rest/bodies/"
+    response = requests.get(url)
+    planets = response.json()['bodies']
+
+  
+    formatted_planets = []
+    for cosmo in planets:
+        if cosmo['isPlanet']:
+            name = cosmo.get('englishName', 'Unknown')  
+            mass = cosmo.get('mass', {}).get('massValue', 'Unknown') 
+            orbit_period = cosmo.get('sideralOrbit', 'Unknown')
+            formatted_planets.append({'name': name, 'mass': mass, 'orbit_period': orbit_period})
+
+    return formatted_planets
+
 def find_heaviest_planet(planets):
-    first_planet = planets[0]
-    planet_with_greater_mass = {'name': first_planet['englishName'], 'mass': first_planet['mass']['massValue']}
-    for planet in planets:
-        if planet['mass'] and planet['mass']['massValue'] >= planet_with_greater_mass['mass']:
-            planet_with_greater_mass = {'name': planet['englishName'], 'mass': planet['mass']['massValue']}
+    heaviest_planet = max(planets, key=lambda p: p['mass'])
+    return heaviest_planet['name'], heaviest_planet['mass']
 
 
 planets = fetch_planet_data()
-name, mass = find_heaviest_planet(planets)
-print(f"The heaviest planet is {name} with a mass of {mass} kg.")
+planet_name, mass = find_heaviest_planet(planets)
+
+
+print(f"{planet_name} is consider the heaviest planet. With a mass of {mass} kg.")
 
